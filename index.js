fe0c99c1abcb367d6e4102340e35e9d343fe9e44
garou <webdev2101@yandex.ru>
2022-03-07 02:10:27 +0400

    newslider-main

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+$(function () {
+  $(".main__items").slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
+});
