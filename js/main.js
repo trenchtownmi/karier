a4d3c34a1b2e9f66dab09968c4235eba7528addb
Mutalibov Said <98184442+trenchtownmi@users.noreply.github.com>
2022-10-25 05:45:09 +0400

    scroll-slow

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(function () {
+
+
+    $(".menu .slider--gallary").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
     
     $('.main__slider').slick({
         infinite: true,
