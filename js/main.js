d8965ae7c88d1ca521fd421537dd1154c60046af
kasumkentskiy <maxummaksumov@yandex.ru>
2022-11-17 05:42:00 +0400

    added typed.js and stellar

--- js/main.js
+++ js/main.js
@@ -1,125 +1,142 @@
 
+
+
+  
+
 $(function () {
 
-    window.onload = function () {
-        let = preloader = document.getElementById('preloader');
-        preloader.style.display = 'none';
-    }
+  window.onload = function () {
+    let = preloader = document.getElementById('preloader');
+    preloader.style.display = 'none';
+  }
 
-    $(".menu .slider--gallary").on("click", function (event) {
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
+  $(".menu .slider--gallary").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
 
-        $('body,html').animate({ scrollTop: top }, 1500);
-    });
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+  
 
-    
-    $('.main__slider').slick({
-        infinite: true,
-        arrows: true,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="left-arrow"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="left-arrow"></button>',
-        responsive: [
-            {
-                breakpoint: 1700,
-                settings: {
-                    arrows: false,
-                },
-            },
-            {
-                breakpoint: 1330,
-                settings: {
-                    arrows: false,
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2, arrows: false,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 690,
-                settings: {
-                    slidesToShow: 1, arrows: false,
-                    slidesToScroll: 1,
-                },
-            },
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ],
-    });
-    ScrollReveal().reveal('.header', {
-        reset: true,
-        // scale: 0.5,
-        distance: '100px',
-        origin: 'top',
-    });
-    ScrollReveal().reveal('article', {
-        reset: true,
-        // scale: 0.5,
-        distance: '100px',
-        origin: 'bottom',
-    });
-    ScrollReveal().reveal('.wellcome', {
-        reset: true,
-        // scale: 0.5,
-        distance: '-100px',
-        origin: 'right',
-    });
-    ScrollReveal().reveal('.album', {
-        reset: true,
-        // scale: 0.5,
-        distance: '-100px',
-        origin: 'right',
-    });
-    ScrollReveal().reveal('.gallary', {
-        reset: true,
-        // scale: 0.5,
-        distance: '100px',
-        origin: 'bottom',
-    });
-    ScrollReveal().reveal('.map', {
-        reset: true,
-        // scale: 0.5,
-        distance: '100px',
-        origin: 'top',
-    });
-    ScrollReveal().reveal('.personal__item', {
-        reset: true,
-        // scale: 0.5,
-        distance: '100px',
-        origin: 'top',
-    });
-    ScrollReveal().reveal('.footer', {
-        reset: true,
-        // scale: 0.5,
-        distance: '-100px',
-        origin: 'right',
-    });
+  $('.main__slider').slick({
+    infinite: true,
+    arrows: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="left-arrow"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="left-arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 1330,
+        settings: {
+          arrows: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2, arrows: false,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 690,
+        settings: {
+          slidesToShow: 1, arrows: false,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+  var typed = new Typed('#typed',{
+    stringsElement: '#typed-strings',
+    typeSpeed: 50,
+   
+  });
+  var typed = new Typed('#typed2',{
+    stringsElement: '#typed-strings2',
+    typeSpeed: 50,
+   
+  });
+  $('.school').stellar();
+  
+  ScrollReveal().reveal('.header', {
+    reset: true,
+    // scale: 0.5,
+    distance: '100px',
+    origin: 'top',
+  });
+  ScrollReveal().reveal('article', {
+    reset: true,
+    // scale: 0.5,
+    distance: '100px',
+    origin: 'bottom',
+  });
+  ScrollReveal().reveal('.wellcome', {
+    reset: true,
+    // scale: 0.5,
+    distance: '-100px',
+    origin: 'right',
+  });
+  ScrollReveal().reveal('.album', {
+    reset: true,
+    // scale: 0.5,
+    distance: '-100px',
+    origin: 'right',
+  });
+  ScrollReveal().reveal('.gallary', {
+    reset: true,
+    // scale: 0.5,
+    distance: '100px',
+    origin: 'bottom',
+  });
+  ScrollReveal().reveal('.map', {
+    reset: true,
+    // scale: 0.5,
+    distance: '100px',
+    origin: 'top',
+  });
+  ScrollReveal().reveal('.personal__item', {
+    reset: true,
+    // scale: 0.5,
+    distance: '100px',
+    origin: 'top',
+  });
+  ScrollReveal().reveal('.footer', {
+    reset: true,
+    // scale: 0.5,
+    distance: '-100px',
+    origin: 'right',
+  });
 
-    $('.menu_btn').on('click', function () {
-        $('.menu').addClass('active');
-    });
-    $('.menu_close').on('click', function () {
-        $('.menu').removeClass('active');
-    });
-    $('.personal__item').on('click', function () {
-        $('.personal__item img').attr('data-fancybox', 'gallary');
-    });
+  $('.menu_btn').on('click', function () {
+    $('.menu').addClass('active');
+  });
+  $('.menu_close').on('click', function () {
+    $('.menu').removeClass('active');
+  });
+  $('.personal__item').on('click', function () {
+    $('.personal__item img').attr('data-fancybox', 'gallary');
+  });
+ 
 })
 
 var mixer = mixitup('.album__content');
@@ -127,13 +144,13 @@ var mixer = mixitup('.album__content');
 // аккордеон
 
 $(document).ready(function () {
-    $('.accordion_title').click(function (event) {
-        if ($('.accordion').hasClass('one')) {
-            $('.accordion_title').not($(this)).removeClass('active');
-            $('.accordion_text').not($(this).next()).slideUp(300);
-        }
-        $(this).toggleClass('active').next().slideToggle(300);
-    });
+  $('.accordion_title').click(function (event) {
+    if ($('.accordion').hasClass('one')) {
+      $('.accordion_title').not($(this)).removeClass('active');
+      $('.accordion_text').not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass('active').next().slideToggle(300);
+  });
 });
 
 
